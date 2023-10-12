17a8df4e0a0e29e66a4efa43e2d934aec9a9efe2
SammyOina <sammyoina@gmail.com>
2023-10-12 15:44:40 +0300

    remove jaeger
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- internal/clients/jaeger/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Mainflux
-// SPDX-License-Identifier: Apache-2.0
-
-// Package jaeger contains the domain concept definitions needed to support
-// Mainflux Jaeger functionality.
-package jaeger
